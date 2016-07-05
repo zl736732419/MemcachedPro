16cc7fa60afcbd8f7817b62d21469c9d94e81f95
zhenglian <zhenglian@DESKTOP-PM0NTSK>
2016-07-05 22:49:23 +0800

    MessageDigest一致性hash实现优化说明

--- src/com/zheng/memcached/consistent/MessageDigestHashFunction.java
+++ src/com/zheng/memcached/consistent/MessageDigestHashFunction.java
@@ -6,6 +6,35 @@ import java.security.NoSuchAlgorithmException;
 
 /**
  * 通过传统的加密方式md5/sha来生成散列的数
+ * 需要说明的是，通过messagedigest生成的是16字节长度的bytes, 那么其实可以分成4组生成4个不同的long
+ * 所以为了充分利用字节数据，可以将虚拟节点分成4个一组，一个虚拟节点名称生成4个对应的hash随机数对应服务器节点
+ * 
+ * nodeName 为当前真实服务器名称
+ * bytes 为生成的16字节加密bytes
+ * virtualNodeNums 为一个服务器节点生成的虚拟节点数，这里假定为64
+ * node为真实添加的服务器节点名次
+ * SortedMap<Long, String> virtualNodes; //为环形上虚拟节点对应的hash排序列表
+ * 
+ * 
+ * byte[] bytes = null;
+ * String virtualNodeName = null;
+ * long num = -1L;
+ * for(int i = 0; i < virtualNodeNums / 4; i++) {
+ * 	virtualNodeName = getVirtualNodeName(nodeName, i); // nodeName + "-" + i;
+ *  bytes = MessageDigestUtils.encrypt(virtualNodeName);
+ *  for(int h = 0; h < 4; h++) {
+ *  	num = ((long)(bytes[3 + 4*h]) << 24)
+ *  		| ((long)(bytes[2 + 4*h) << 16)
+ *  		| ((long)(bytes[1 + 4*h) << 8)
+ *  		| (long)(bytes[0] & 0xff);
+ *  
+ *  	virtualNodes.put(num, nodeName);
+ *  }
+ * 
+ * }
+ * 
+ * 
+ * 
  * @author zhenglian
  *
  */
